98bbf3d9f25f97c830fae95c08d88a1955b085e3
Aditya Vaidya <kroq.gar78@gmail.com>
2012-11-10 18:33:38 -0600

    featext-automated.sh: simplified the sed process

--- featext-automated.sh
+++ featext-automated.sh
@@ -1,12 +1,7 @@
 #/bin/bash -eu
-rlength=$((`find . -type d -not -path './.git*' -not -path './ECG_FEATURE_WAVELET_ST*' -not -name 'timestr' -not -name '.'|wc -l`))
 records=`find . -type d -not -path './.git*' -not -path './ECG_FEATURE_WAVELET_ST*' -not -name 'timestr' -not -name '.'|sort`
-#for i in `echo $records|xargs`; do
-for (( i = 2; i <= $rlength; i+=1)); do
-	rnum=`echo $records| cut -d" " -f$i`
-	rnum=${rnum:2}
-	rnum_old=`echo $records| cut -d" " -f$((i-1))`
-	rnum_old=${rnum_old:2}
-	sed -i "s/rnum\ \=\ '$rnum_old'/rnum\ \=\ '$rnum'/g" featExt.m
+for i in `echo $records|xargs`; do
+	rnum=${i:2}
+	sed -i "s/rnum\ \=\ '[0-9][0-9][0-9]'/rnum\ \=\ '$rnum'/g" featExt.m
 	matlab -glnx86 -r "path(path,'.'); featExt; exit" -nodesktop -nosplash
 done
