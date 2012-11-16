d939128fd40238aa58499312dd204a69647fdf70
Aditya Vaidya <kroq.gar78@gmail.com>
2012-11-15 22:38:02 -0600

    mat2csv.py: modified to get all points' csv's

--- mat2csv.py
+++ mat2csv.py
@@ -11,22 +11,35 @@ rnum = 100
 
 # read the .mat file
 mat = scipy.io.loadmat(str(rnum)+'_r.mat')
-vals = []
+
 #for i in mat['Rloc']:
 #	vals[0].append(int(i))
 #for i in mat['Ramp']:
 #	vals[1].append(int(i))
 
-for i in xrange(len(mat['Rloc'])):
-	vals.append([int(mat['Rloc'][i]),int(mat['Ramp'][i])])
-
 print vals
 
 # write the data into a .csv file
 rfile = open((str(rnum)+"_r.csv"),'wb')
+pfile = open((str(rnum)+"_p.csv"),'wb')
+qfile = open((str(rnum)+"_q.csv"),'wb')
+sfile = open((str(rnum)+"_s.csv"),'wb')
+tfile = open((str(rnum)+"_t.csv"),'wb')
 rwriter = csv.writer(rfile, dialect="excel")
+pwriter = csv.writer(pfile, dialect="excel")
+qwriter = csv.writer(qfile, dialect="excel")
+swriter = csv.writer(sfile, dialect="excel")
+twriter = csv.writer(tfile, dialect="excel")
 
-for i in vals:
-	rwriter.writerow([i[0],i[1]])
+for i in xrange(len(mat['Rloc'])):
+	rwriter.writerow([int(mat['Rloc'][i]),int(mat['Ramp'][i])])
+for i in xrange(len(mat['Ploc'])):
+	pwriter.writerow([int(mat['Ploc'][i]),int(mat['Pamp'][i])])
+for i in xrange(len(mat['Qloc'])):
+	qwriter.writerow([int(mat['Qloc'][i]),int(mat['Qamp'][i])])
+for i in xrange(len(mat['Sloc'])):
+	swriter.writerow([int(mat['Sloc'][i]),int(mat['Samp'][i])])
+for i in xrange(len(mat['Tloc'])):
+	rwriter.writerow([int(mat['Tloc'][i]),int(mat['Tamp'][i])])
 
 rfile.close()
