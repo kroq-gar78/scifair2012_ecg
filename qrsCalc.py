5176093896920ee4c0985548109d05c3036c853f
Aditya Vaidya <kroq.gar78@gmail.com>
2012-11-17 16:01:08 -0600

    qrsCalc.py: now loads all values into arrays

--- qrsCalc.py
+++ qrsCalc.py
@@ -2,15 +2,23 @@
 
 import csv
 
+# needed values from JBiSE:
+# PR (probably PQ; P to (R or Q))
+# QRS (Q to S)
+# QT (Q to T)
+# ST (S to T)
+
 rnum = 100
 
-rfile = csv.reader(open(str(rnum)+"/"+str(rnum)+"_r.csv",'rb'))
+# use "with open('filename') as f:" later on instead of this
 pfile = csv.reader(open(str(rnum)+"/"+str(rnum)+"_p.csv",'rb'))
 qfile = csv.reader(open(str(rnum)+"/"+str(rnum)+"_q.csv",'rb'))
+rfile = csv.reader(open(str(rnum)+"/"+str(rnum)+"_r.csv",'rb'))
 sfile = csv.reader(open(str(rnum)+"/"+str(rnum)+"_s.csv",'rb'))
 tfile = csv.reader(open(str(rnum)+"/"+str(rnum)+"_t.csv",'rb'))
 
-diffs = []
+# this stuff is heart rate...
+'''diffs = []
 rownum = 0
 prevq = 0
 curq = 0
@@ -28,8 +36,55 @@ for row in rfile:
 			curq = float(col)
 			print "done"
 		colnum += 1
-	rownum += 1
+	rownum += 1'''
+
+# translate the locations data into arrays
+
+ploc = []
+qloc = []
+rloc = []
+sloc = []
+tloc = []
+
+for row in pfile:
+	colnum = 0
+	for col in row:
+		if colnum != 0: continue
+		ploc.append(int(col))
+		colnum += 1
+
+for row in qfile:
+	colnum = 0
+	for col in row:
+		if colnum != 0: continue
+		qloc.append(int(col))
+		colnum += 1
+		
+for row in rfile:
+	colnum = 0
+	for col in row:
+		if colnum != 0: continue
+		tloc.append(int(col))
+		colnum += 1
+
+for row in sfile:
+	colnum = 0
+	for col in row:
+		if colnum != 0: continue
+		sloc.append(int(col))
+		colnum += 1
+		
+for row in tfile:
+	colnum = 0
+	for col in row:
+		if colnum != 0: continue
+		tloc.append(int(col))
+		colnum += 1
 
 #qfile.close()
 
-print diffs
+print ploc
+print qloc
+print rloc
+print sloc
+print tloc
