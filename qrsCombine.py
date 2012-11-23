c5bfbfb8c36539d174921aa33b9953bb5db12253
Aditya Vaidya <kroq.gar78@gmail.com>
2012-11-23 00:24:18 -0600

    qrsCombine.py: calculate heart rate, write all data to a single file

--- qrsCombine.py
+++ qrsCombine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Get the average, maximum, and minimum QRS complex length
+# Get the heart rate, average, maximum, and minimum QRS complex length
 
 import csv
 import subprocess # for system commands
@@ -24,6 +24,27 @@ for rnum in ecglist:
 			tmplist.append(int(row[0]))
 		qrslist.append(tmplist)
 
+# calculate heart rate (R peak to R peak interval) after reading
+rintv = []
+for rnum in ecglist:
+	with open(str(rnum)+"/"+str(rnum)+"_r.csv",'rb') as f:
+		tmpcsv = csv.reader(f)
+		# read
+		rlist = []
+		for row in tmpcsv:
+			rlist.append(int(row[0]))
+		#print rlist
+		# calculate
+		tmplist = []
+		for j in xrange(len(rlist)-1):
+			#print rlist[j+1],rlist[i]
+			tmplist.append(rlist[j+1]-rlist[j])
+		#rintv.append(tmplist)
+		rate = sum(tmplist)/len(tmplist)
+		rintv.append(rate)
+		#rintv[rnum] = rate
+#print rintv
+
 # calculate averages, maximums, and minimums
 qrsavg = []
 qrsmax = []
@@ -32,3 +53,11 @@ for i in xrange(len(qrslist)):
 	qrsavg.append(sum(qrslist[i])/sum(qrslist[i]))
 	qrsmax.append(max(qrslist[i]))
 	qrsmin.append(min(qrslist[i]))
+
+# write all rates, avgs, maxs, mins to a single CSV
+with open('all_qrsnn.csv','wb') as f:
+	qrsnnWriter = csv.writer(f, dialect="excel")
+	for i in xrange(len(ecglist)):
+		rnum = ecglist[i]
+		#with open(str(rnum)+"/"+str(rnum)+"_qrsnn.csv",'wb') as f:
+		qrsnnWriter.writerow([rintv[i],qrsavg[i],qrsmax[i],qrsmin[i]])
