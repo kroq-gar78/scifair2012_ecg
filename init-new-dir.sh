3fd17aae04922868927fdbb39362f906a6133502
Aditya Vaidya <kroq.gar78@gmail.com>
2012-12-02 15:46:47 -0600

    init-new-dir.sh: add mat2csv.sh and qrsCalc.sh

--- init-new-dir.sh
+++ init-new-dir.sh
@@ -10,7 +10,7 @@ for i in `find ../ -maxdepth 1 -name '*.mat'|grep -e "[0-9][0-9][0-9]m\.mat"|sed
 done
 
 # symlink all other scripts/programs except "featExt.m"
-for i in mk-ecg-dirs.sh mat-mv-to-dirs.sh mat2csv.py qrsCalc.py qrsCombine.py featext-automated.sh featext-matlab-cli.sh csv-list.sh csv-mv-to-dirs.sh librow_winmax.m find-ecg-dirs.sh find-ecgs.sh; do
+for i in mk-ecg-dirs.sh mat-mv-to-dirs.sh mat2csv.py qrsCalc.py qrsCombine.py featext-automated.sh featext-matlab-cli.sh csv-list.sh csv-mv-to-dirs.sh librow_winmax.m find-ecg-dirs.sh find-ecgs.sh mat2csv.sh qrsCalc.sh; do
     ln -s ../$i .
 done
 
