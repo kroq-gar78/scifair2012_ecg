698a7b3d23c0ea1701c40a05889fda0afffc5472
Aditya Vaidya <kroq.gar78@gmail.com>
2012-12-02 15:25:08 -0600

    init-new-dir.sh: copy instead of symlink 'featExt.m' because it's modified during the run process

--- init-new-dir.sh
+++ init-new-dir.sh
@@ -9,7 +9,10 @@ for i in `find ../ -maxdepth 1 -name '*.mat'|grep -e "[0-9][0-9][0-9]m\.mat"|sed
     ln -s ../$i$strend .
 done
 
-# symlink all other scripts/programs
-for i in featExt.m mk-ecg-dirs.sh mat-mv-to-dirs.sh mat2csv.py qrsCalc.py qrsCombine.py featext-automated.sh featext-matlab-cli.sh csv-list.sh csv-mv-to-dirs.sh; do
+# symlink all other scripts/programs except "featExt.m"
+for i in mk-ecg-dirs.sh mat-mv-to-dirs.sh mat2csv.py qrsCalc.py qrsCombine.py featext-automated.sh featext-matlab-cli.sh csv-list.sh csv-mv-to-dirs.sh; do
     ln -s ../$i .
 done
+
+# copy instead of symlink "featExt.m" because it is modified during the run
+cp -va ../featExt.m ./
