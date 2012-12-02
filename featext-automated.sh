6974718208ddbd27c5a2ed8db1b3c0502bc97f11
Aditya Vaidya <kroq.gar78@gmail.com>
2012-12-02 01:19:37 -0600

    featext-automated.sh: print the record number for easier tracking

--- featext-automated.sh
+++ featext-automated.sh
@@ -1,6 +1,7 @@
 #/bin/bash -eu
 records=`find . -maxdepth 1 -name '*.mat'|grep -e "[0-9][0-9][0-9]m\.mat"|sed 's/m\.mat//g'|sed 's/\.\///g'|sort`
 for i in `echo $records|xargs`; do
+	echo $i
 	sed -i "s/rnum\ \=\ '[0-9][0-9][0-9]'/rnum\ \=\ '$i'/g" featExt.m
 	matlab -glnx86 -r "path(path,'.'); featExt; exit" -nodesktop -nosplash
 done
