f373b7bf1656fe6d6ddfa91f4f7b19e70f78c2a5
Aditya Vaidya <kroq.gar78@gmail.com>
2012-12-05 18:37:09 -0600

    featext-automated.sh: updated to use './find-ecgs.sh'

--- featext-automated.sh
+++ featext-automated.sh
@@ -1,5 +1,5 @@
 #/bin/bash -eu
-records=`find . -maxdepth 1 -name '*.mat'|grep -e "[0-9][0-9][0-9]m\.mat"|sed 's/m\.mat//g'|sed 's/\.\///g'|sort`
+records=`./find-ecgs.sh`
 for i in `echo $records|xargs`; do
 	echo $i
 	sed -i "s/rnum\ \=\ '[0-9][0-9][0-9]'/rnum\ \=\ '$i'/g" featExt.m
